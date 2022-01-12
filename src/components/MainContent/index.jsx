7bc388afd3899563fdd20abae995fbe6df8b9b39
Lê Minh Toàn <mmminhle771@gmail.com>
2022-01-12 12:05:29 +0700

    hide create post when logout

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -5,71 +5,79 @@ import * as actions from '../../redux/actions';
 import { authState$, postsState$ } from '../../redux/selectors';
 import './mainContent.scss';
 
-
 function MainContent() {
     const dispatch = useDispatch();
     const { data, isLoading } = useSelector(postsState$);
     const { userId } = useSelector(authState$);
 
+    const handleUpdatePost = useCallback(
+        (postId, contentEdit, setIsEdit) => {
+            dispatch(
+                actions.updatePost.updatePostRequest({
+                    _id: postId,
+                    content: contentEdit,
+                })
+            );
+            setIsEdit(false);
+        },
+        [dispatch]
+    );
 
+    const handleDeletePost = useCallback(
+        (_id, setIsDelete) => {
+            dispatch(actions.deletePost.deletePostRequest(_id));
+            setIsDelete(false);
+        },
+        [dispatch]
+    );
 
-    const handleUpdatePost = useCallback((postId, contentEdit, setIsEdit) => {
-        dispatch(actions.updatePost.updatePostRequest({
-            _id: postId,
-            content: contentEdit
-        }));
-        setIsEdit(false);
-    }, [dispatch]);
+    const handleLikePost = useCallback(
+        (postId, userId, currentLike) => {
+            if (userId) {
+                let newLike;
 
-    const handleDeletePost = useCallback((_id, setIsDelete) => {
-        dispatch(actions.deletePost.deletePostRequest(_id));
-        setIsDelete(false);
-    }, [dispatch])
+                if (currentLike.includes(userId)) {
+                    newLike = currentLike.filter((item) => item !== userId);
+                } else {
+                    newLike = [...currentLike, userId];
+                }
 
-    const handleLikePost = useCallback((postId, userId, currentLike) => {
-        if (userId) {
-            let newLike;
-
-            if (currentLike.includes(userId)) {
-                newLike = currentLike.filter(item => item !== userId);
-            } else {
-                newLike = [...currentLike, userId];
+                dispatch(
+                    actions.updatePost.updatePostRequest({
+                        _id: postId,
+                        likeCount: newLike,
+                    })
+                );
             }
-
-            dispatch(actions.updatePost.updatePostRequest({
-                _id: postId,
-                likeCount: newLike
-            }))
-        }
-    }, [dispatch]);
+        },
+        [dispatch]
+    );
 
     return (
         <>
-            <section className="content">
-                <BoxCreate />
-                <div className="content__list">
+            <section className='content'>
+                {userId && <BoxCreate />}
+                <div className='content__list'>
                     {isLoading ? (
                         <Loading />
+                    ) : data.length > 0 ? (
+                        data.map((post) => (
+                            <PostItem
+                                key={post._id}
+                                data={post}
+                                userId={userId}
+                                onUpdatePost={handleUpdatePost}
+                                onDeletePost={handleDeletePost}
+                                onLike={handleLikePost}
+                            />
+                        ))
                     ) : (
-                        data.length > 0 ? (
-                            data.map(post => (
-                                <PostItem
-                                    key={post._id}
-                                    data={post}
-                                    userId={userId}
-                                    onUpdatePost={handleUpdatePost}
-                                    onDeletePost={handleDeletePost}
-                                    onLike={handleLikePost}
-                                />
-                            ))
-                        ) : (
-                            <p className="content__nothing">You don't have any posts yet.</p>
-                        )
+                        <p className='content__nothing'>You don't have any posts yet.</p>
                     )}
                 </div>
             </section>
         </>
-    )
+    );
 }
 
-export default MainContent
+export default MainContent;
